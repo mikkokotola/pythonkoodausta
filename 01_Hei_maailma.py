b39e710ae58990cdc6a5a737e3694002e443593b
Mikko Kotola <mikko.kotola@sympa.com>
2019-11-20 18:48:51 +0200

    Tehtävät 1-4

---
+++ 01_Hei_maailma.py
@@ -0,0 +1,4 @@
+# Tehtävä 1
+# Kirjoita ohjelma, joka tulostaa tekstin "Hei maailma!" 
+
+print ('')
---
+++ 03_Heippa.py
@@ -0,0 +1,6 @@
+# Tehtävä 3
+# Kirjoita ohjelma, joka kysyy käyttäjän nimen ja tulostaa sitten tekstin "Moikka xx" (eli esim. "Moikka Mauri") .
+# Käytä apunasi muuttujaa "teksti". Kokeile ajaa ohjelma useita kertoja ja syötä eri nimi.
+
+teksti = input("")
+print ("" + teksti)
